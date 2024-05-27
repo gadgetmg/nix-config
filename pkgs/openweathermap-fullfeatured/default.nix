9573814c72b4eb85ab2ee81dd6445c592c9e4546
Matt Graham <gadgetmg@pm.me>
2024-05-27 10:03:59 -0400

    Fix weather script to include runtime dependencies

--- pkgs/openweathermap-fullfeatured/default.nix
+++ pkgs/openweathermap-fullfeatured/default.nix
@@ -1,4 +1,5 @@
-{ lib, writeShellApplication }: writeShellApplication {
+{ pkgs, lib, writeShellApplication }: writeShellApplication {
   name = "openweathermap-fullfeatured.sh";
+  runtimeInputs = with pkgs; [ coreutils curl jq ];
   text = builtins.readFile ./openweathermap-fullfeatured.sh;
 }
